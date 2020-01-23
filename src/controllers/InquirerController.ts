0006c646ed86c8290d6e66dcaba5c1469b047ddc
Andre Pato <anpato1994@gmail.com>
2020-01-22 21:49:12 -0500

    added test axios call, verifed repos clone correctly

--- src/controllers/InquirerController.ts
+++ src/controllers/InquirerController.ts
@@ -1,19 +1,75 @@
-import { writeFile } from 'fs'
+import { writeFile, mkdir } from 'fs'
+import { promisify } from 'util'
+import { exec } from 'child_process'
+import Axios from 'axios'
 export default class InquirerController {
+  private token: string
+  private username: string
+  private githubType: string
+  private exec: any
   constructor(private prompt: any, private prompts: object[]) {
     this.prompt = prompt
     this.prompts = prompts
+    this.token = ''
+    this.username = ''
+    this.githubType = ''
+    this.exec = promisify(exec)
   }
 
   private writeTokenToEnv = (token: string): void => {
+    this.token = token
+    this.prompts.splice(0, 2)
     writeFile(`${process.cwd()}/./.env`, `GITHUB_TOKEN=${token}`, err => {
       if (err) throw err
+      this.setUpGithubConnection(token)
     })
+  }
+
+  private fetchRepos = async () => {
+    try {
+      const resp = await Axios.get(
+        `https://git.generalassemb.ly/api/v3/users/${this.username}/repos`,
+        {
+          headers: {
+            Authorization: `token ${this.token}`
+          }
+        }
+      )
+      await this.cloneRepos(resp.data)
+    } catch (error) {
+      throw error
+    }
+  }
+
+  private cloneRepos = async (repos: object[]) => {
+    mkdir(`${process.cwd()}/./repos`, async err => {
+      if (err) return
+    })
+    repos.forEach(async (repo: any) => {
+      try {
+        await this.exec(
+          `cd ./repos && git clone ${repo.clone_url}`,
+          (err: any) => {
+            if (err) {
+            }
+          }
+        )
+      } catch (error) {
+        return
+      }
+    })
+  }
+
+  private setUpGithubConnection = async (token: string) => {
+    const typeOfGithub = await this.prompt(this.prompts)
+    console.log(typeOfGithub)
   }
 
   public intializePrompt = async () => {
     const answers = await this.prompt(this.prompts)
-    const { token } = answers
-    this.writeTokenToEnv(token)
+    const { token, username } = answers
+    this.token = token
+    this.username = username
+    await this.fetchRepos()
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,29 @@ const initialPrompt: interfaces.Prompt = {
     url
   )} to create a new Github Personal Access Token. Press enter/return once completed.`
 }
-const getInfoPrompt: interfaces.Prompt = {
+const getTokenPrompt: interfaces.Prompt = {
   name: 'token',
   message: 'Enter your oauth token'
 }
 
-const prompts: object[] = [initialPrompt, getInfoPrompt]
+const getGithubUrl: interfaces.Prompt = {
+  type: 'list',
+  name: 'url',
+  message: 'Is this an enterprise Github account or public Github account?',
+  choices: ['Public', 'Enterprise']
+}
+
+const getUsername: interfaces.Prompt = {
+  name: 'username',
+  message: "Whats's your github username?"
+}
+
+const prompts: object[] = [
+  initialPrompt,
+  getGithubUrl,
+  getTokenPrompt,
+  getUsername
+]
 
 const Inquirer = new InquirerController(prompt, prompts)
 Inquirer.intializePrompt()
