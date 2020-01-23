a60baf854398e48a9cda1ab675f3347b30c07f8f
Andre Pato <anpato1994@gmail.com>
2020-01-22 20:55:11 -0500

    write github token to env file from prompt

--- src/controllers/InquirerController.ts
+++ src/controllers/InquirerController.ts
@@ -1,13 +1,19 @@
-import { prompt } from 'inquirer'
-
+import { writeFile } from 'fs'
 export default class InquirerController {
   constructor(private prompt: any, private prompts: object[]) {
     this.prompt = prompt
     this.prompts = prompts
   }
 
+  private writeTokenToEnv = (token: string): void => {
+    writeFile(`${process.cwd()}/./.env`, `GITHUB_TOKEN=${token}`, err => {
+      if (err) throw err
+    })
+  }
+
   public intializePrompt = async () => {
     const answers = await this.prompt(this.prompts)
-    console.log(answers)
+    const { token } = answers
+    this.writeTokenToEnv(token)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import { prompt } from 'inquirer'
 import InquirerController from './controllers/InquirerController'
 import * as interfaces from './middleware/interfaces'
+import * as chalk from 'chalk'
+
+const url = new URL('https://github.com/settings/tokens')
 
 const initialPrompt: interfaces.Prompt = {
   name: 'initialize',
-  message: 'Hey there, welcome to Super Clone!'
+  message: `Hey there, welcome to Super Clone! To get started visit ${chalk.green(
+    url
+  )} to create a new Github Personal Access Token. Press enter/return once completed.`
 }
 const getInfoPrompt: interfaces.Prompt = {
   name: 'token',
   message: 'Enter your oauth token'
 }
 
-const Inquirer = new InquirerController(prompt, [initialPrompt, getInfoPrompt])
+const prompts: object[] = [initialPrompt, getInfoPrompt]
+
+const Inquirer = new InquirerController(prompt, prompts)
 Inquirer.intializePrompt()
